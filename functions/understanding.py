aa8da5a9f42ed8be76842d581c233c47ef7b8d3c
Samkelwa <samkelwa101@gmail.com>
2025-02-03 22:54:09 +0200

    added some notes to the 'understanding' file.

--- functions/understanding.py
+++ functions/understanding.py
@@ -1,3 +1,10 @@
 # Understanding functions               02/02/2025                  23:00
 
-       
+# Giving functions descriptive names helps us to know at  glance what tehy do.
+print("Opening example")
+def get_final_price(price, tax):
+    return price + tax
+
+price = get_final_price(30, 1.5)
+print(price)
+
