d522b6197737ef9c2b3c802c97016030a2cac8fa
Samkelwa <samkelwa101@gmail.com>
2025-02-13 23:07:05 +0200

    Added a new file, completed the previous notes on dictionaries

--- tuples-dictionaries-sets/dictionaries2.py
+++ tuples-dictionaries-sets/dictionaries2.py
@@ -58,13 +58,85 @@ tickets = {
     "seat no.": 25
 }
 
-ticket["window"] = True
-print(ticket)
+tickets["window"] = True
+print(tickets)
 # Adding a new key-value pair resembles updating, with the difference being that the key is not yet in the dictionary
 
 
 
-# Using Dictionaries (PART 3)               11/02/2025
+# Using Dictionaries (PART 3)               13/02/2025                       17:32
+
+# To check if a dictionary contains a certain key, we use the 'in' keyword
+print("     ")
+print("Example 3.1: Using the 'in' keyword")
+
+personal_data = {
+    "name": "Mac Miller",
+    "telephone": "0047865791"
+}
+
+print("name" in personal_data)
+# As with lists, the keyword gives True when the key is present, and False otherwise
+
+
+# We can store the information of whether a key is present in the dictionary in a variable
+print("     ")
+print("Example 3.2: Storing the result of the 'in' keyword in a variable")
+
+has_address = "address" in personal_data
+print(has_address)
+
+
+
+# Using Dictionaries (PART 4)               13/02/2025                        17:06
+
+# We remove a key-value pair similarly to removing an element from a list, by coding the dictionary name and then teh '.pop()' instruction
+print("     ")
+print("Example 4.1: Removing a key-value pair from a dictionary")
+
+stock = {
+    "dresses": 25,
+    "t-shirts": 50,
+    "jeans": 1
+}
+
+# stock.pop()
+# print(stock)
+
+
+# When using '.pop()' with dictionaries, we need to specify which key we want to remove
+print("     ")
+print("Example 4.2: Specifying a key to remove")
+
+stock.pop("jeans")
+print(stock)
+
+
+# We can store the value of the removed key in a variable
+print("     ")
+print("Example 4.3: Storing the value of the removed key in a variable")
+
+jeans_stock = stock.pop("jeans")
+print(jeans_stock)
+
+
+# Attempting to remove a key that doesn't exist gives us an error
+print("     ")
+print("Example 4.4: Attempting to remove a key that doesn't exist")
+
+# stock.pop("caps")
+
+
+# To avoid getting an error when removing a key, it's good practice to first use the 'in' keyword to check if the key exists
+print("     ")
+print("Example 4.5: Using the 'in' keyword to check if a key exists in a dictionary")
+
+if "dresses" in stock:
+    stock.pop("dresses")
+    
+print(stock)
+
+
 
 
 
---
+++ tuples-dictionaries-sets/sets1.py
@@ -0,0 +1,27 @@
+# Creating Sets                             13/02/2025                      17:41
+
+# When we want to make sure that a collection of values can't have any duplicates, we store it in a set
+print("     ")
+print("Opening Example: an example of a set")
+
+postcodes = {"SW1A", "SY3", "B44"}
+print(postcodes)
+
+
+# Sets are collections of values like lists, but they do not allow for any duplicate values
+# To create a set, we start with curly braces {} to surround the set values
+# We seperate set values using commas   ,   
+# To reuse a set, we can store it in a variable
+
+
+print("     ")
+print("EXAMPLES")
+
+# 1
+print("- - - - - - - ")
+print("1")
+
+cities = {"Rio", "Denver", "Moscow", "Berlin"}
+
+print(cities)
+
