f8eb64b91b2eee4235ec3c87371afac54bd5a241
Samkelwa <samkelwa101@gmail.com>
2025-04-10 18:01:22 +0200

    Mostly done with negIndexing and del, few lines left.

--- string-list_operations/negIndexing _n_deletion.py
+++ string-list_operations/negIndexing _n_deletion.py
@@ -34,7 +34,7 @@ print("Example 3: Modifying using negative indexing")
 
 white_users = ["Jack", "Achmed", "Elaine"]
 
-users[-2] = "Jill"
+white_users[-2] = "Jill"
 print(white_users)
 
 # Tuples are also ordered data structures and values can be retreived, but they are immutable and so cannot be modified
@@ -63,6 +63,67 @@ print(winners)
 letters = {"a", "b", "c"}
 
 del letters
-# print(letters)                - Trying to print the "letters" dictionary will result in an error since we deleted it
+# print(letters)                - Trying to print the "letters" set will result in an error since we deleted it
 
- 
+
+# We can also del a key inside a dictionary to remove unwanted key:value pairs from the dictionary
+print(" ")
+print("Example 6: Removing a key-value pair from a dictionary ")
+
+product = {
+    "category": "book",
+    "price": 4.99,
+    "in_shop": True
+}
+print(product)
+
+del product["in_shop"]
+print(product)
+
+
+print("EXAMPLES")
+
+# 1
+print("- - - - -")
+print("1")
+
+user = ("Joe", "Paris", 27)
+name = user[0]
+age = user[-1]
+message = f"Hello, {name}, your are {age} years old."
+print(message)
+
+
+# 2
+print("- - - - - ")
+print("2")
+
+users = ["Peter", "Zaneta", "Irina"]
+print(users)
+users[-1] = "Paul"
+print(users)
+
+
+# 3
+print("- - - - - ")
+print("3")
+
+orders = ["A", "B", "C"]
+print(orders)
+del orders[-1]
+print(orders)
+
+
+# 4
+print("- - - - -")
+print("4")
+
+details = {
+    "name": "Alex",
+    "age": 22,
+    "gender": "Female"
+}
+print(details)
+
+del details["gender"]
+print(details)
